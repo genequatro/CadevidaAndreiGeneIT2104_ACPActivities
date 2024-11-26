6418867858b42eeebd9df29e7044d594858dfc8c
genequatro <23-01234@g.batstate-u.edu.ph>
2024-11-26 09:15:15 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 4 - 26-11-24/Exception Handling/lab4_item2.py
@@ -0,0 +1,17 @@
+try:
+  size = int(input("Enter the size of the array: "))
+
+  arr = [0.0] * size
+
+  print("Enter the elements of the array: ")
+
+  for i in range(size):
+    arr[i] = float(input())
+
+  x = int(input("Enter the index of the element: "))
+
+  print(f"Element at index {x}: {arr[x]:.2f}")
+
+except IndexError:
+  print(f"Index {x} is invalid.")
+
